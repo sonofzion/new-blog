a90ef31c15a5f5671acbc7e52c1a0111161f05ba
sonofzion <damilojualajo@gmail.com>
2024-01-22 14:14:52 +0100

    fix

--- src/Usercomponent1.js
+++ src/Usercomponent1.js
@@ -5,7 +5,7 @@ import React from 'react';
 const UserComponent1 = () => {
   return (
     <div className="user-component">
-      <h3>User Component 1</h3>
+      <h3>User Component 1</h3>git branch -m <main></main>
      <p3>great mind are always on a set goals!</p3>
     </div>
   );
